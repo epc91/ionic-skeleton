860e7eb0c49a29eee54dde8ee990d8ba53845756
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-19 00:36:34 -0400

    Init Project Working

---
+++ ionic-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ionic start name(camelCase) template(tabs || sidemenu || blank) --type=(angular | react | vue)
+
+# ionic generate type(pages || components || directives || services) name, options
+# full list of types: npx ng g --help
+# full list of options for types: npx ng g <type> --help
+
+### SCRIPT
+
+echo --- A simple script to create a Ionic project ---
+echo 
+echo Start blank app with Angular
+
+ionic start skeletonApp blank --type=angular
+
+echo Generate a Page type
+
+cd skeletonApp
+ionic generate page login
+
+ionic serve
